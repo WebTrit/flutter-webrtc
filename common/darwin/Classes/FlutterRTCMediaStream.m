232f9a6019000460ceda442aa4c30eacef4a4712
Yurii Cherniavskyi <ych@webtrit.com>
2023-09-12 15:01:31 +0300

    fix: add virtual Speaker audio output port after real one
    This allow to check is the Speaker audio output option is actually selected
    or is available in the list as a selectable option.

--- common/darwin/Classes/FlutterRTCMediaStream.m
+++ common/darwin/Classes/FlutterRTCMediaStream.m
@@ -643,6 +643,12 @@ typedef void (^NavigatorUserMediaSuccessCallback)(RTCMediaStream* mediaStream);
 
   for (AVAudioSessionPortDescription* port in session.currentRoute.outputs) {
     // NSLog(@"output portName: %@, type %@", port.portName,port.portType);
+    [sources addObject:@{
+      @"deviceId" : port.UID,
+      @"label" : port.portName,
+      @"groupId" : port.portType,
+      @"kind" : @"audiooutput",
+    }];
     if (session.currentRoute.outputs.count == 1 && ![port.UID isEqualToString:@"Speaker"]) {
       [sources addObject:@{
         @"deviceId" : @"Speaker",
@@ -651,12 +657,6 @@ typedef void (^NavigatorUserMediaSuccessCallback)(RTCMediaStream* mediaStream);
         @"kind" : @"audiooutput",
       }];
     }
-    [sources addObject:@{
-      @"deviceId" : port.UID,
-      @"label" : port.portName,
-      @"groupId" : port.portType,
-      @"kind" : @"audiooutput",
-    }];
   }
 #endif
 #if TARGET_OS_OSX
