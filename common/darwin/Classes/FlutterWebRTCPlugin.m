c997bf276190d8268665aad538af310a0cb27877
Vladislav Komelkov <digiborideveloper@gmail.com>
2025-02-03 17:16:47 +0200

    fix: ensure speaker on during session init (#1)

--- common/darwin/Classes/FlutterWebRTCPlugin.m
+++ common/darwin/Classes/FlutterWebRTCPlugin.m
@@ -1445,7 +1445,7 @@ static FlutterWebRTCPlugin *sharedSingleton;
 
 - (void)ensureAudioSession {
 #if TARGET_OS_IPHONE
-  [AudioUtils ensureAudioSessionWithRecording:[self hasLocalAudioTrack]];
+  [AudioUtils ensureAudioSessionWithRecording:[self hasLocalAudioTrack] andSpeaker:_speakerOn];
 #endif
 }
 
